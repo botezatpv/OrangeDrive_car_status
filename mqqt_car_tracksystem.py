3f09ab21bd11dfd1a6425483def679f3bacb12eb
Pavel Botezat <botezatpv@gmail.com>
2016-07-30 22:41:14 +0300

    Trying to connect edison to relay module

--- mqqt_car_tracksystem.py
+++ mqqt_car_tracksystem.py
@@ -1,1 +1,6 @@
-print 'Im working bitches'
+from paho.mqqt import client
+
+if connect(host, port=1883, keepalive=60, bind_addres="") == True:
+	print 'I have connected Bitch!'
+else:
+	print 'NOOOOO IM DEAD MAN'
