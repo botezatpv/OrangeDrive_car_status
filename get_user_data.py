8beb585c099e5993ae1030b7633fdbd75d10585f
spacesoldier <kibersoldier@gmail.com>
2016-07-31 05:44:06 +0300

    send sms

--- get_user_data.py
+++ get_user_data.py
@@ -1,5 +1,9 @@
 import requests
 
-r = requests.get("http://104.154.26.64/gate?name=Andrey&id=1")
+r = requests.get('http://104.154.26.64/gate?name=Andrey&id=1')
 
 print(r.json)
+
+r2 = requests.post('http://104.155.156.203/sendSMS(+79265342055).json', data={'Rawpostdata','Heol%20wrold!!!'})
+print('\n')
+print(r2.text)
