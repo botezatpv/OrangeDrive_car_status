e80897a893fa39d3ad8f287ef7ab562b941adc44
Pavel Botezat <botezatpv@gmail.com>
2016-07-31 13:42:15 +0300

    Gsm script v2

--- gsm_modem_handler.py
+++ gsm_modem_handler.py
@@ -5,7 +5,7 @@ Demo: handle incoming SMS messages by replying to them
 Simple demo app that listens for incoming SMS messages, displays the sender's number
 and the messages, then replies to the SMS by saying "thank you"
 """
-
+"""
 from __future__ import print_function
 
 import logging
@@ -37,3 +37,42 @@ def main():
 
 if __name__ == '__main__':
     main()
+"""
+import serial
+import time
+
+class TextMessage:
+    def __init__(self, recipient="+2348065777685", message="TextMessage.content not set."):
+        self.recipient = recipient
+        self.content = message
+
+    def setRecipient(self, number):
+        self.recipient = number
+
+    def setContent(self, message):
+        self.content = message
+
+    def connectPhone(self):
+        self.ser = serial.Serial('COM70', 460800, timeout=5, xonxoff = False, rtscts = False, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE)
+        time.sleep(1)
+
+    def sendMessage(self):
+        self.ser.write('ATZ\r')
+        time.sleep(1)
+        self.ser.write('AT+CMGF=1\r')
+        time.sleep(1)
+        self.ser.write('''AT+CMGS="''' + self.recipient + '''"\r''')
+        time.sleep(1)
+        self.ser.write(self.content + "\r")
+        time.sleep(1)
+        self.ser.write(chr(26))
+        time.sleep(1)
+
+    def disconnectPhone(self):
+        self.ser.close()
+
+sms = TextMessage("+2348063796720","Mummy i sent this message from my computer")
+sms.connectPhone()
+sms.sendMessage()
+sms.disconnectPhone()
+print "message sent successfully"
