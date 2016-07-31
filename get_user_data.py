f3f77042e1617ae709cde0b42f1a7dfb03928164
spacesoldier <kibersoldier@gmail.com>
2016-07-31 15:32:58 +0300

    Get request to server

--- get_user_data.py
+++ get_user_data.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 r = requests.get('http://104.154.26.64/gate?name=Andrey&id=1')
 
@@ -9,3 +10,7 @@ print('\n')
 print(r2.text)
 r3 = requests.request('POST','http://104.155.156.203/sendSMS(+79265342055).json?Rawpostdata=Heol%20wrold!!!')
 print(r3.text)
+
+
+response = requests.get('http://orangedrive.ru/gate?rq=open_car&user_id=1&lat=33&lng=35')
+json_data = json.loads(response.text)
