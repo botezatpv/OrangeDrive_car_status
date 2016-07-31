9c2d5f719c378ce7305af3b6d2b9ddbd35cf0bba
spacesoldier <kibersoldier@gmail.com>
2016-07-31 06:16:13 +0300

    111

--- get_user_data.py
+++ get_user_data.py
@@ -2,7 +2,7 @@ import requests
 
 r = requests.get('http://104.154.26.64/gate?name=Andrey&id=1')
 
-print(r.json)
+print(r.text)
 
 r2 = requests.post('http://104.155.156.203/sendSMS(+79265342055).json', data={'Rawpostdata':'Heol%20wrold!!!'})
 print('\n')
