f9d7f9be91f14a1909d5556e330807b92328fc65
Pavel Botezat <botezatpv@gmail.com>
2016-08-04 03:31:56 +0300

    sending message to MQTT

--- gsm_modem_handler.py
+++ gsm_modem_handler.py
@@ -15,13 +15,14 @@ BAUDRATE = 115200
 PIN = None # SIM card PIN (if any)
 
 from gsmmodem.modem import GsmModem
-
+import paho.mqtt.publish as publish
 def handleSms(sms):
     print(u'== SMS message received ==\nFrom: {0}\nTime: {1}\nMessage:\n{2}\n'.format(sms.number, sms.time, sms.text))
     print('Replying to SMS...')
     sms.reply(u'SMS received: "{0}{1}"'.format(sms.text[:20], '...' if len(sms.text) > 20 else ''))
     print('SMS sent.\n')
-    print('HI Its Me Mario')
+    msgs = [{'topic':"/hello/Lol", 'payload':"0"}
+    publish.single(msgs, hostname = '10.0.0.49')
     
 def main():
     print('Initializing modem...')
@@ -38,48 +39,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-"""
-import serial
-import time
-
-class TextMessage:
-    def __init__(self, recipient="+79671358170", message="TextMessage.content not set."):
-        self.recipient = recipient
-        self.content = message
-
-    def setRecipient(self, number):
-        self.recipient = number
-
-    def setContent(self, message):
-        self.content = message
-
-    def connectPhone(self):
-        self.ser = serial.Serial('/dev/ttyUSB0', 460800, timeout=5, xonxoff = False, rtscts = False, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE)
-        time.sleep(1)
-
-    def sendMessage(self):
-        self.ser.write('ATZ\r')
-        time.sleep(1)
-        self.ser.write('AT+CMGF=1\r')
-        time.sleep(1)
-        self.ser.write('''AT+CMGS="''' + self.recipient + '''"\r''')
-        time.sleep(1)
-        self.ser.write(self.content + "\r")
-        time.sleep(1)
-        self.ser.write(chr(26))
-        time.sleep(1)
-    def getMessage(self):
-        return self.ser.write('AT+CMGR=2\r')
-       
-
-    def disconnectPhone(self):
-        self.ser.close()
-
-sms = TextMessage("+79671358170","Mummy i sent this message from my computer")
-sms.connectPhone()
-#sms.sendMessage()
-sms.getMessage()
-sms.disconnectPhone()
-print sms.getMessage()
-#print "message sent successfully"
-"""
