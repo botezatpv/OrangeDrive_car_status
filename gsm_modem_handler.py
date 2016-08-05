35418507a90e2e0cc9d2fbd3bf413ec64e5a6699
botezatpv <botezatpv@gmail.com>
2016-08-05 08:42:56 +0300

    transformed into classes

--- gsm_modem_handler.py
+++ gsm_modem_handler.py
@@ -1,56 +1,40 @@
 #!/usr/bin/env python
-
-
-"""Demo: handle incoming SMS messages by replying to them
-Simple demo app that listens for incoming SMS messages, displays the sender's number'
-and the messages, then replies to the SMS by saying "thank you"
-"""
-
 from __future__ import print_function
 import logging
-
-try:
-    import paho.mqtt.publish as publish
-except ImportError:
-    # This part is only required to run the example from within the examples
-    # directory when the module itself is not installed.
-    #
-    # If you have the module installed, just use "import paho.mqtt.publish"
-    import os
-    import inspect
-    from delay import delayed
-    import time
-    cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../src")))
-    if cmd_subfolder not in sys.path:
-        sys.path.insert(0, cmd_subfolder)
-    import paho.mqtt.publish as publish
-
-PORT = '/dev/ttyUSB2'
-BAUDRATE = 115200
-PIN = None # SIM card PIN (if any)
-
+import paho.mqtt.publish as publish
 from gsmmodem.modem import GsmModem
 
-def handleSms(sms):
-    print(u'== SMS message received ==\nFrom: {0}\nTime: {1}\nMessage:\n{2}\n'.format(sms.number, sms.time, sms.text))
-    print('Replying to SMS...')
-    sms.reply(u'SMS received: "{0}{1}"'.format(sms.text[:20], '...' if len(sms.text) > 20 else ''))
-    print('SMS sent.\n')
-    publish.single("hello/","Hello", hostname="10.0.0.4")
-    
-    
-def main():
-    print('Initializing modem...')
-    # Uncomment the following line to see what the modem is doing:
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
-    modem = GsmModem(PORT, BAUDRATE, smsReceivedCallbackFunc=handleSms)
-    modem.smsTextMode = False 
-    modem.connect(PIN)
-    print('Waiting for SMS message...')    
-    try:    
-        modem.rxThread.join(2**31) # Specify a (huge) timeout so that it essentially blocks indefinitely, but still receives CTRL+C interrupt signal
-    finally:
-        modem.close();
+class GsmModemHandler:
+	#Initializing PORT, BAUDRATE, PIN, HOSTNAME
+	def __init__(self):
+		self.PORT = '/dev/ttyUSB2'
+		self.BAUDRATE = 115200
+		self.PIN = None # SIM card PIN (if any)
+		self.HOSTNAME = "10.0.0.4"
+
+	#Get and reply sms	
+	def handleSms(self, sms):
+	    print(u'== SMS message received ==\nFrom: {0}\nTime: {1}\nMessage:\n{2}\n'.format(sms.number, sms.time, sms.text))
+	    print('Replying to SMS...')
+	    sms.reply(u'SMS received: "{0}{1}"'.format(sms.text[:20], '...' if len(sms.text) > 20 else ''))
+	    print('SMS sent.\n')
+	    publish.single("hello/","Hello", hostname=self.HOSTNAME)
+	    return sms.text
+	    
+	#Connecting to gsm device    
+	def main(self):
+	    print('Initializing modem...')
+	    # Uncomment the following line to see what the modem is doing:
+	    #logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+	    modem = GsmModem(self.PORT, self.BAUDRATE, smsReceivedCallbackFunc=self.handleSms)
+	    modem.smsTextMode = False 
+	    modem.connect(self.PIN)
+	    print('Waiting for SMS message...')    
+	    try:    
+	        modem.rxThread.join(2**31) # Specify a (huge) timeout so that it essentially blocks indefinitely, but still receives CTRL+C interrupt signal
+	    finally:
+	        modem.close();
 
 if __name__ == '__main__':
-    main()
+	gsm = GsmModemHandler()
+	gsm.main()
--- mqqt_car_tracksystem.py
+++ mqqt_car_tracksystem.py
@@ -1,29 +1,30 @@
 import paho.mqtt.client as mqtt
-import paho.mqtt.publish as publish
 
+class MqttClient:
 
+	def __init__(self):
+		self.HOSTNAME = "10.0.0.49"
+		self.PORT = 1883
+		self.KEEPALIVE = 60
+		self.client = mqtt.Client()
 
+	# The callback for when the client receives a CONNACK response from the server
+	# Subscribe on topic when client receives a CONNACK response from the server
+	def on_connect(self, client, userdata, flags, rc):
+	    print("Connected with result code "+str(rc))
+	    self.client.subscribe("#")
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+	# The callback for when a PUBLISH message is received from the server.
+	def on_message(self, client, userdata, msg):
+	    print(msg.topic+" "+str(msg.payload))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("#")
+	def main(self):    
+		self.client.on_connect = self.on_connect
+		self.client.on_message = self.on_message
+		self.client.connect(self.HOSTNAME, self.PORT, self.KEEPALIVE)
+		self.client.loop_forever()
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+if __name__ == "__main__":
+	mqqt = MqttClient()
+	mqqt.main()
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect("10.0.0.49", 1883, 60)
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
