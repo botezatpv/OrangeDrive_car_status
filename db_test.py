ef9bb29066dbbe4f587db98b7e6a3ef2bbec6976
botezatpv <botezatpv@gmail.com>
2016-08-06 13:41:54 +0300

    Added QrReader

---
+++ db_test.py
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/python
+from tarantool import Connection
+import types
+
+c = Connection(host = "104.154.26.64", port = 3301)
+#user = 'usr', password = '123', 
+print c._recv
---
+++ qr_scanner.py
@@ -0,0 +1,8 @@
+from qrtools import QR
+class ScanFromCamera:
+	def scan(self):
+		myCode = QR()
+		print myCode.decode_webcam()
+if __init__=="__main__":
+	sc = ScanFromCamera
+	sc.scan()
