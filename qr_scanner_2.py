cfda3f5ed54fc7006311ce013a2288010f79593d
botezatpv <botezatpv@gmail.com>
2016-08-06 22:04:04 +0300

    hw

--- qr_scanner_2.py
+++ qr_scanner_2.py
@@ -4,14 +4,14 @@ import zbar
 def scanner_procces(frame,set_zbar):    
     set_width = 100.0 / 100
     set_height = 90.0 / 100
-   
+    
     coord_x = int(frame.width * (1 - set_width)/2)
     coord_y = int(frame.height * (1 - set_height)/2)
     width = int(frame.width * set_width)
     height = int(frame.height * set_height)
-   
+   	
     get_sub = cv.GetSubRect(frame, (coord_x+1, coord_y+1, width-1, height-1))
-   
+    
     cv.Rectangle(frame, (coord_x, coord_y), (coord_x + width, coord_y + height), (255,0,0))
    
     cm_im = cv.CreateImage((get_sub.width, get_sub.height), cv.IPL_DEPTH_8U, 1)
@@ -23,7 +23,7 @@ def scanner_procces(frame,set_zbar):
             print '\033[1;32mResult : %s symbol "%s" \033[1;m' % (symbol.type,symbol.data)
            
     cv.ShowImage("webcame", frame)
-    #cv.ShowImage("webcame2", get_sub)
+    cv.ShowImage("webcame2", get_sub)
     cv.WaitKey(10)
  
    
