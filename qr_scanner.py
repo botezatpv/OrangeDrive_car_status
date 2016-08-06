bc8d03c085ec766bba6f434c422ebcf9cd3ff534
botezatpv <botezatpv@gmail.com>
2016-08-06 17:21:58 +0300

    next try

--- qr_scanner.py
+++ qr_scanner.py
@@ -142,7 +142,7 @@ def getIntersection(a1,a2,b1,b2,intersection):
 	intersection = (int(p[0]+(t*r[0])),int(p[1]+(t*r[1])))
 	return True,intersection
 
-camera = PiCamera()
+camera = cv2.VideoCapture(0)
 camera.resolution = (640,480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera,size=(640,480))
