071d740309a9af540ebf3e803d133c8e95ac934e
botezatpv <botezatpv@gmail.com>
2016-08-06 18:33:20 +0300

    trying to load camera form cv2

--- qr_scanner_2.py
+++ qr_scanner_2.py
@@ -1,7 +1,3 @@
-﻿import qrtools
-from qrtools import QR
-myCode = QR()
-print myCode.decode_webcam()
-print myCode.data
-print myCode.data_type
-print myCode.data_to_string()
+﻿import cv2
+
+camera = cv2.VideoCapture(0)
